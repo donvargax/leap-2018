c896b8434cd74ab50c8e8c6d1c266b2fe69bbbdb
Jorge Vargas <yorch.veintemil+github@gmail.com>
2018-04-04 06:55:48 -0700

    feat(config): read config file and start dogfooding

---
+++ .uaasrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  version: 1,
+  type: "js",
+  source_branch: "master",
+  target_branch: "master",
+  dependencies_branch_prefix: "dependencies",
+  reviewers: [
+    "daniel.luna@thermofisher.com"
+  ],
+  update: "every monday at 6AM",
+  yarn_version: "1.5.1",
+}
--- src/bin/cli.tsx
+++ src/bin/cli.tsx
@@ -2,6 +2,16 @@ import { h, renderToString } from "ink";
 
 import { Green } from "../lib/green";
 
+import cosmiconfig = require("cosmiconfig")
+
+const { config = {} } =
+    cosmiconfig("uaas", {
+        rcExtensions: true,
+        sync: true
+    }).load()
+
+console.log("CONFIG", JSON.stringify(config))
+
 process.stdout.write(
   renderToString(
     <Green>
